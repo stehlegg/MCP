d3ba49f217bbe44c103fc8a2b9335cd498da04b7
stehlegg <29704323+stehlegg@users.noreply.github.com>
2020-08-23 20:10:36 +0200

    initial commit

---
+++ src/main/java/core/Main.java
@@ -0,0 +1,14 @@
+package core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	@Override
+	public void onEnable()  {
+		getLogger().info("YA YEETICUS");
+	}
+	@Override
+	public void onDisable()  {
+		getLogger().info("YA OOFDICUS");
+	}
+}
